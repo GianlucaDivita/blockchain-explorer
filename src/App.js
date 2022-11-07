889ce2402ed46a561b70adef801c662f62267fd3
Sarim Sohail <6844166+s4r1m@users.noreply.github.com>
2022-11-06 23:02:15 -0500

    add route

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Route, Link, Routes } from "react-router-dom"
-import {Component} from "react"
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from "react";
-import Transactions from './Components/Transactions';
-import Addresses from './Components/Addresses';
-import Wallet from './Components/Wallet';
-import Navigation from './Components/Navigation';
-import Home from './Components/Home';
-
+import Transactions from "./Components/Transactions";
+import Addresses from "./Components/Addresses";
+import Wallet from "./Components/Wallet";
+import { Transfer } from "./Components/Transfer";
+import Navigation from "./Components/Navigation";
+import Home from "./Components/Home";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <div>
-        <Navigation/>
-        <Routes>
-        <Route path="/" element={<Home/>} exact/>
-        <Route path="/Transactions" element={<Transactions/>} />
-        <Route path="/Addresses" element={<Addresses/>} />
-        <Route Component={Error} />
-        <Route path="/Wallet" element={<Wallet/>} />
-      </Routes>
-      </div>
+        <div>
+          <Navigation />
+          <Routes>
+            <Route path="/" element={<Home />} exact />
+            <Route path="/Transactions" element={<Transactions />} />
+            <Route path="/Addresses" element={<Addresses />} />
+            <Route path="/Wallet" element={<Wallet />} />
+            <Route path="/Transfer/:receiver" element={<Transfer />} />
+          </Routes>
+        </div>
       </BrowserRouter>
-
     </div>
   );
 }
