e6465b251ef0edc44cd9ea707c25b44dfaa0c88f
Sarim Sohail <6844166+s4r1m@users.noreply.github.com>
2022-11-20 21:09:09 -0500

    add mongoose

--- server/app.js
+++ server/app.js
@@ -1,6 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+
 const app = express();
 const port = 3000;
+
+mongoose.connect("mongodb://mongo:27017", { useNewUrlParser: true }).then(
+  () => {
+    console.log("Mongoose Connected Successfully");
+  },
+  (error) => {
+    console.log("Mongoose could not connect: " + error);
+  }
+);
+
 app.listen(port, () => {
   console.log(`Server listening on ${port}`);
 });
