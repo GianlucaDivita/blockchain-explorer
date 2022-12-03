6eea37d6859ed74c4046acc08eb322925213a790
Sarim Sohail <6844166+s4r1m@users.noreply.github.com>
2022-12-03 02:28:43 -0500

    post request to make a transfer

--- client/src/Components/Transfer.js
+++ client/src/Components/Transfer.js
@@ -6,17 +6,39 @@
 The component will show a mock Transfer Receipt after the Submit action if clicked
 */
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./transfer.css";
 import { useParams } from "react-router-dom";
 import Receipt from "./Receipt";
+import axios from "axios";
 
 export const Transfer = () => {
   const { receiver } = useParams();
   const [transactions, setTransactions] = useState([]);
   const [amount, setAmount] = useState("");
+  const [account, setAccount] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:3001/addresses")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data[0]);
+        setAccount(data[0]);
+      });
+  }, []);
 
   const transact = () => {
+    axios
+      .post("http://localhost:3001/sendTransaction", {
+        getsd: "dfsd",
+        source: account,
+        destination: receiver,
+        value: amount,
+      })
+      .then((response) => console.log(response.data))
+      .catch(function (error) {
+        console.log(error);
+      });
     setTransactions((prev) => {
       return [...prev, { receiver: receiver, amount: amount }];
     });
@@ -31,9 +53,7 @@ export const Transfer = () => {
       <h3 className="wallet-title">Transfer</h3>
 
       <div className="transfer-box">
-        <h3 className="address">
-          From: 0x4C9E4585Bd7623Db96Dd544D9A3f99aA05DB7876
-        </h3>
+        <h3 className="address">From: {account}</h3>
         <h3 className="address">To: {receiver}</h3>
         <input className="text" onChange={handleChange}></input>
         <br />
