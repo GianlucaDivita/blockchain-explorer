733ba2486dfded4a39a9de0c2b4c4f66d1532f2f
Gianluca Di Vita <gianlucajdivita@gmail.com>
2022-12-05 22:48:17 -0500

    3D Cube to home screen

---
+++ client/src/Components/cube.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./cube.css";
+
+function Cube() {
+    return ( 
+
+        <div className="cube-obj">
+        <div class="cube-1">
+        <div class="top-1"></div>
+
+    <div className= "cube-body">
+      <span className="zero"></span>
+      <span className="one"></span>
+      <span className="two"></span>
+      <span className="three"></span>
+   </div>
+        
+    </div>
+
+</div>
+
+     );
+}
+
+export default Cube;
